dd420a62e9b969c624c52988d569073680b50536
Lopez Lopez <43147@rpsk12.org>
2024-03-21 12:45:05 -0400

    message here

---
+++ js.js
@@ -0,0 +1,89 @@
+// var boxOne = document.getElementsByClassName('box1')[0];
+// var running = false;
+
+// var toggleTransition = function() {
+//   if(!running) 
+//   { 
+//     boxOne.classList.add('horizTranslate');
+//   } else {
+//     boxOne.classList.remove('horizTranslate'); 
+//   }  
+
+//   running = !running;
+// }
+
+// var boxOne2 = document.getElementsByClassName('box2')[0];
+// var running2 = false;
+
+// var toggleTransition2 = function() {
+//   if(!running2) 
+//   { 
+//     boxOne2.classList.add('horizTranslate2');
+//   } else {
+//     boxOne2.classList.remove('horizTranslate2'); 
+//   }  
+
+//   running2 = !running2;
+// }
+
+// example3
+
+// var boxOne3 = document.getElementsByClassName('box3')[0];
+// var running3 = false;
+
+// var toggleTransition3 = function() {
+//   if(!running3) 
+//   { 
+//     boxOne3.classList.add('horizTranslate3');
+//   } else {
+//     boxOne3.classList.remove('horizTranslate3'); 
+//   }  
+
+//   running3 = !running3;
+// }
+
+// example4
+// var boxOne4 = document.getElementsByClassName('box4')[0];
+// var running4 = false;
+
+// var toggleTransition4 = function() {
+//   if(!running4) 
+//   { 
+//     boxOne4.classList.add('horizTranslate4');
+//   } else {
+//     boxOne4.classList.remove('horizTranslate4'); 
+//   }  
+
+//   running4 = !running4;
+// }
+
+// example5
+
+var boxOne51 = document.getElementsByClassName('box51')[0];
+var running51 = false;
+
+var toggleTransition51 = function() {
+  if(!running51) 
+  { 
+    boxOne51.classList.add('horizTranslate51');
+  } else {
+    boxOne51.classList.remove('horizTranslate51'); 
+  }  
+
+  running51 = !running51;
+}
+// example6
+
+var boxOne6 = document.getElementsByClassName('box6')[0];
+var running6 = false;
+
+var toggleTransition6 = function() {
+  if(!running6) 
+  { 
+    boxOne6.classList.add('horizTranslate6');
+  } else {
+    boxOne6.classList.remove('horizTranslate6'); 
+  }  
+
+  running6 = !running6;
+}
